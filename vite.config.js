8b375401ca1a563f7b2440900e16fae3bd8b9327
Dusan Zivanovic <dusanzivanovic990@gmail.com>
2023-03-08 19:59:08 +0100

    initial project setup with vite,pug,scss and ts

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite"
+import pugPlugin from "vite-plugin-pug"
+
+const options = { pretty: true } // FIXME: pug pretty is deprecated!
+const locals = { name: "My Pug" }
+
+export default defineConfig({
+   plugins: [pugPlugin({ localImports: true }, { hello: "world", baseUrl: "/" })],
+   base: "./",
+   build: {
+      rollupOptions: {
+         input: {
+            main: "../src/index.html",
+         }
+      }
+   }
+})
