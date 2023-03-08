6ce67081a130cb913988a8e4350d9d226a129101
Dusan Zivanovic <dusanzivanovic990@gmail.com>
2023-03-08 21:43:38 +0100

    added header

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import "./styles/main.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+   const navigationLinks = document.querySelectorAll('.main_nav ul li a');
+   const navigationList = document.querySelectorAll('.main_nav ul li');
+   const currentPage = document.location.href;
+   
+   for (let i = 0; i < navigationLinks.length; i++) {
+      currentPage === navigationLinks[i].href ? navigationList[i].className += ' active' : ''
+	}
+ });
