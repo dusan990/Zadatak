f9124623a5c9f76cf772196083206539f1966f8b
Dusan Zivanovic <dusanzivanovic990@gmail.com>
2023-03-10 09:41:12 +0100

    added mobile, meta

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,44 @@
 import "./styles/main.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-   const navigationLinks = document.querySelectorAll('.main_nav ul li a');
-   const navigationList = document.querySelectorAll('.main_nav ul li');
-   const currentPage = document.location.href;
-   
-   for (let i = 0; i < navigationLinks.length; i++) {
-      currentPage === navigationLinks[i].href ? navigationList[i].className += ' active' : ''
-	}
- });
+   const navLinks = document.querySelectorAll('.main_nav ul li a');
+   const navList = document.querySelectorAll('.main_nav ul li');
+   const currentUrl = window.location.href;
+
+   for (let i = 0; i < navLinks.length; i++) {
+      // currentPage === navigationLinks[i].href ? navigationList[i].className += ' active' : ''
+      var linkUrl = navLinks[i].href;
+
+      if ((currentUrl.indexOf(linkUrl) !== -1) || (currentUrl.endsWith("/index.html") || currentUrl.endsWith("/")) && i === 0) {
+         // Add the "active" class to the navigation link
+         navList[i].classList.add('active');
+      }
+   }
+});
+
+
+let slideIndex:number = 1;
+showSlides(slideIndex);
+
+function showSlides(n:number) {
+   const slides:HTMLCollectionOf<Element> = document.getElementsByClassName("product-slider-slide");
+
+   n > slides.length ? slideIndex = 1 : null
+   n < 1 ? slideIndex = slides.length : null
+
+   for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+   }
+
+   slides[slideIndex - 1].style.display = "block";
+}
+
+const prevSlideBtn = document.getElementById("prev");
+const nextSlideBtn = document.getElementById("next")
+
+nextSlideBtn!.addEventListener('click', () => {
+   showSlides(slideIndex += 1);
+})
+prevSlideBtn!.addEventListener('click', () => {
+   showSlides(slideIndex - 1);
+})
