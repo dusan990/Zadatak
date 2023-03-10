293db173f6be0a64c41b38f5f6db6ded587c6777
Dusan Zivanovic <dusanzivanovic990@gmail.com>
2023-03-10 11:58:22 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,23 @@ const options = { pretty: true } // FIXME: pug pretty is deprecated!
 const locals = { name: "My Pug" }
 
 export default defineConfig({
-   plugins: [pugPlugin({ localImports: true }, { hello: "world", baseUrl: "/" })],
+   plugins: [pugPlugin(options, locals, { hello: "world", baseUrl: "/" })],
    base: "./",
    build: {
       rollupOptions: {
+         outDir: 'dist',
+         assetsDir: 'src/assets',
+         minify: true,
+         sourcemap: false,
          input: {
-            main: "../src/index.html",
-         }
+            input: 'src/main.ts',
+         },
+         output: {
+            format: 'es',
+            entryFileNames: '[name].[hash].js',
+            chunkFileNames: '[name].[hash].js',
+            assetFileNames: '[name].[hash].[ext]',
+          },
       }
-   }
+   },
 })
